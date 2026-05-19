fdf673d5f501102db66838e957ae8a11da5a4866
Dario Mauro <sitiwebdm78@gmail.com>
2026-05-19 17:36:09 +0200

    Terminata  User Story 9

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -5,13 +5,15 @@ namespace App\Jobs;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Spatie\Image\Image;
+use Spatie\Image\Enums\CropPosition;
+use Spatie\Image\Enums\Unit;
 
 class ResizeImage implements ShouldQueue
 {
     use Queueable;
-
+    
     private $w, $h, $path, $fileName;
-
+    
     public function __construct($filePath, $w, $h)
     {
         $this->path = dirname($filePath);
@@ -19,18 +21,24 @@ class ResizeImage implements ShouldQueue
         $this->w = $w;
         $this->h = $h;
     }
-
+    
     public function handle(): void
     {
-        $srcPath = storage_path("app/public/{$this->path}/{$this->fileName}");
-        $destPath = storage_path("app/public/{$this->path}/crop_{$this->w}x{$this->h}_{$this->fileName}");
-        
-        if (!file_exists($srcPath)) {
-            return;
-        }
+        $w = $this->w;
+        $h = $this->h;
+        $srcPath = storage_path() . "/app/public/" . $this->path . "/" . $this->fileName;
+        $destPath = storage_path() . "/app/public/" . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
         
         Image::load($srcPath)
-            ->crop($this->w, $this->h)
-            ->save($destPath);
+        ->crop($w, $h, CropPosition::Center)
+        ->watermark(
+        base_path('resources/img/watermark.png'),
+        width: 50,
+        height: 50,
+        paddingX: 5,
+        paddingY: 5,
+        paddingUnit: Unit::Percent
+        )
+        ->save($destPath);
     }
 }
